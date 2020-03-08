11e0c0c72983d01e19147a14d58bd88877b38be9
devtesta <s4908752@bournemouth.ac.uk>
2020-03-08 18:26:11 +0000

    Operators

---
+++ operators/arithmeticOperators.js
@@ -0,0 +1,25 @@
+
+let x = 10;
+let y = 3;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+// Mod
+console.log(x % y);
+// X to power y 
+console.log(x ** y);
+
+// Increment After
+console.log(x++);
+console.log(x);
+// Increment Before
+console.log(++x);
+
+// Decrement After
+console.log(x--);
+console.log(x);
+
+// Decrement Before
+console.log(--x);
---
+++ operators/logicalOperators.js
@@ -0,0 +1,25 @@
+// Logical AND (&&)
+// Returns TRUE if both operands are TRUE
+console.log(true && true)
+
+let highIncome = true;
+let goodCreditScore = true;
+
+let eligibileForLoan = highIncome && goodCreditScore;
+
+console.log(eligibileForLoan)
+
+// Logical OR (||)
+// Returns TRUE if one of the operands are TRUE
+console.log(true || true)
+
+highIncome = false;
+goodCreditScore = false;
+
+eligibileForLoan = highIncome || goodCreditScore;
+
+console.log(eligibileForLoan)
+
+// Logical NOT (!)
+let applicationRefused = !eligibileForLoan;
+console.log('Application Refused: ' + applicationRefused);
